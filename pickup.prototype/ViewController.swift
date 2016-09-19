5b60a1ca3dcc941c314e4003640946d907d74ddb
fargofremling <alicia.fremling@gmail.com>
2016-09-18 20:35:10 -0700

    Initial Commit

---
+++ pickup.prototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pickup.prototype
+//
+//  Created by Alicia Fremling on 9/18/16.
+//  Copyright (c) 2016 Alicia Fremling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
