76832d59b27dc279cafb8c87e3ca419447df284f
fargofremling <alicia.fremling@gmail.com>
2016-09-21 23:02:16 -0800

    Practice adding new Cocoa Class

---
+++ Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  
+//
+//  Created by Alicia Fremling on 9/22/16.
+//
+//
+
+import Cocoa
+
+class Login: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
